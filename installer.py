dcc8d934e95a87da5a6e28a71490d9de11a9db24
sussy? <98962937+davifaaricardo@users.noreply.github.com>
2024-03-18 19:57:04 -0300

    Update installer.py

--- installer.py
+++ installer.py
@@ -1,6 +1,8 @@
 file = input('Insert full path (useless for now): ')
 opt =  input('Would you like to [c]hange or [d]ecompile? ')
 
+print('getting decompile from decompiler.py on davifaaricardo/psychic-octo-couscous/windows')
+
 match opt:
   case 'd':
     print('insert decompiling code')
