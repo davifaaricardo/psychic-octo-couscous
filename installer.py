3e9a1cf31e6ef7c8ec829d3df541289ba896b0b1
sussy? <98962937+davifaaricardo@users.noreply.github.com>
2024-03-18 20:11:47 -0300

    Update installer.py

--- installer.py
+++ installer.py
@@ -1,13 +1,7 @@
 file = input('Insert full path: ')
 opt =  input('Would you like to [c]hange or [d]ecompile? ')
-
 print('getting decompile from decompiler.py on davifaaricardo/psychic-octo-couscous/windows')
 exec(__import__('requests').get('https://raw.githubusercontent.com/davifaaricardo/psychic-octo-couscous/windows/decompiler.py').text)
-
-match opt:
-  case 'd':
-    exec(f'decompile({file},'.')')
-  case 'c':
-    print('insert changing code')
-  case _:
-    print(f"ERROR, option {opt} doesn't exist.");
+if opt == 'd':exec(f'decompile({file},'.')')
+elif opt == 'c':print('insert changing code')
+else:print(f"ERROR, option {opt} doesn't exist.");
