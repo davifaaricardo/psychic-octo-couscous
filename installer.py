69908e6d80f9bf49faa61bfd94a75e537946b105
sussy? <98962937+davifaaricardo@users.noreply.github.com>
2024-03-18 19:41:56 -0300

    Update installer.py

--- installer.py
+++ installer.py
@@ -1,10 +1,1 @@
-file = input('Insert full path (useless for now): ')
-opt =  input('Would you like to [c]hange or [d]ecompile? ')
-
-match opt:
-  case 'd':
-    print('insert decompiling code')
-  case 'c':
-    print('insert changing code')
-  case _:
-    print(f"ERROR, option {opt} doesn't exist.");
+print('deleted')
