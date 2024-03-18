a236dda4a70b18922308c9a793d5d9533a4c4054
sussy? <98962937+davifaaricardo@users.noreply.github.com>
2024-03-17 23:26:09 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import platform,requests
-try:
-  OS = {'Windows': 'windows', 'Linux': 'linux', 'Darwin': 'mac-OS'}[platform.system()]
-  os_gh_repo = f'https://github.com/davifaaricardo/psychic-octo-couscous/{OS}/installer.py'
-  GET_Request = requests.get(os_gh_repo)
-  print(GET_Request.text())
-  exec(GET_Request.text())
-except:
-  print('Most accurate error: Unknown OS, please manually download from the branches: Windows; linux; Mac-OS ')
-  exit()
+try: import platform except: print('Please install `platform` '); exit()
+try: import requests except: print('Please install `requests` '); exit()
+try: OS = {'Windows': 'windows', 'Linux': 'linux', 'Darwin': 'mac-OS'}[platform.system()] except: print(' Syntax Error;\n you changed some code in platform;\n Most accurate error: Unknown OS, please manually download from the branches: Windows; linux; Mac-OS'); exit()
+try: os_gh_repo = f'https://raw.githubusercontent.com/davifaaricardo/psychic-octo-couscous/{OS}/installer.py' except: print('error in OS'); exit()
+try: GET_Request = requests.get(os_gh_repo) except: print('Unknown link or error in `os_gh_repo`'); exit()
+try: exec(GET_Request.text) except: print(f'error in `{OS}/install.py` or error in GET_Request'); exit()
