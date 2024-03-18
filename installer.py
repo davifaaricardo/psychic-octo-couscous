6e26b9759aa7d37aeaa48007024fe0952491ca6b
sussy? <98962937+davifaaricardo@users.noreply.github.com>
2024-03-18 19:36:51 -0300

    Update installer.py

--- installer.py
+++ installer.py
@@ -1,1 +1,12 @@
-file = input('Insert full path')
+file = input('Insert full path: ')
+opt =  input('Would you like to [c]hange or [d]ecompile? ')
+import colorama
+colorama.init(autoreset)
+
+match opt:
+  case 'd':
+    print('insert decompiling code')
+  case 'c':
+    print('insert changing code')
+  case _:
+    print(f"{colorama.Fore.RED}ERROR{colorama.Fore.RESET} option: {opt} doesn't exist.")
