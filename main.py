2774400bbcbe7f6b0008fdbcaea270e675aa75f2
sussy? <98962937+davifaaricardo@users.noreply.github.com>
2024-03-17 23:13:50 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
-print('Please wait, loading libraries')
-
-import platform
-import requests
-
-OSChoice = {'Windows': 'windows', 'Linux': 'linux', 'Darwin': 'mac-OS'}
+import platform,requests
 try:
-  OS = OSChoice[platform.system()]
+  OS = {'Windows': 'windows', 'Linux': 'linux', 'Darwin': 'mac-OS'}[platform.system()]
+  os_gh_repo = f'https://github.com/davifaaricardo/psychic-octo-couscous/{OS}/installer.py'
+  GET_Request = requests.get(os_gh_repo)
+  print(GET_Request.text())
+  exec(GET_Request.text())
 except:
   print('Most accurate error: Unknown OS, please manually download from the branches: Windows; linux; Mac-OS ')
   exit()
-os_gh_repo = f'https://github.com/davifaaricardo/psychic-octo-couscous/{OS}/installer.py'
-GET_Request = requests.get(os_gh_repo)
-print(GET_Request.text())
-exec(GET_Request.text())
